bb5b9c8abbb4aa003332dc6cc03c0c4540a14cff
Prateek Varma <prtk.varma@gmail.com>
2022-06-29 15:38:42 -0600

    initial commit, installed some npm dependencies, and scaffolded Reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from "redux";
+
+export default combineReducers({
+  // Below, just a dummy func to return something, so Redux doesn't give errors during intial stages of building the app.
+  replaceMe: () => "Dummy text",
+});
