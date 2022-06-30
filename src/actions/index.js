98af4443ce193e14a4568b039853d272ae0ad2cc
Prateek Varma <prtk.varma@gmail.com>
2022-06-30 17:59:51 -0600

    Formatted the Action Creator to use Redux-Thunk by returning a function with async/await API call and dispatch() the payload

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,11 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-//FOLLOWING IS A BAD EXAMPLE OF MAKING AN REQUEST FROM AN EXTERNAL API - FROM AN ACTION CREATOR
-//THE NEXT COMMIT WILL FIX THIS.
-export const fetchPosts = async () => {
-    const response = await jsonPlaceholder.get('/posts')
-
-  return {
-    type: "FETCH_POSTS",
-    payload: response
+//Below, we're returning a function inside the ACTION CREATOR. A Function or XXX are the only 2 things that an ACTION CREATOR can return, when using REACT-THUNK.
+//Also, we're able to use async/await because we're using REACT THUNK. Otherwise, this an async/await synctax would give an error on a usual ACTION CREATOR.
+export const fetchPosts = () => {
+  // Below, the function can have 2 params, (dispatch, getState) but we don't need the getState in this scenario.
+  return async function (dispatch) {
+    const response = await jsonPlaceholder.get("/posts");
+    dispatch({ type: "FETCH_POSTS", payload: response });
   };
 };
