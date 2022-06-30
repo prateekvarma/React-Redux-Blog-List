377fe77d698c46461981f95ebadd9a208876120b
Prateek Varma <prtk.varma@gmail.com>
2022-06-29 18:08:57 -0600

    *BAD EXAMPLE* of making an API request from an Action Creator.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,12 @@
-//Below is just a dummy 'action creator', so we can see it render
-export const fetchPosts = () => {
+import jsonPlaceholder from "../apis/jsonPlaceholder";
+
+//FOLLOWING IS A BAD EXAMPLE OF MAKING AN REQUEST FROM AN EXTERNAL API - FROM AN ACTION CREATOR
+//THE NEXT COMMIT WILL FIX THIS.
+export const fetchPosts = async () => {
+    const response = await jsonPlaceholder.get('/posts')
+
   return {
     type: "FETCH_POSTS",
+    payload: response
   };
 };
